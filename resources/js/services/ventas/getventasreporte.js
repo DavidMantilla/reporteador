8af85873d0921a27326cd78cd35e69676f2a199c
David Mantilla <david.mantilla@gopass.com.co>
2025-05-29 11:46:18 -0500

    prueba

--- resources/js/services/ventas/getventasreporte.js
+++ resources/js/services/ventas/getventasreporte.js
@@ -10,7 +10,9 @@ async function getVentas(url) {
     try {
         console.log(`${config.OdataUrl}ventas?$expand=sucursales${url}`);
 
-        const response = await axios.get(`${config.OdataUrl}ventas?$expand=sucursales${url}`);
+        const response = await axios.get(
+            `${config.OdataUrl}ventas?$expand=sucursales${url}`
+        );
         return response;
     } catch (error) {
         console.error("Error fetching users:", error);
@@ -29,29 +31,57 @@ function inicializeventasTable(jsonData) {
                         data: "FechaDoc",
                         render: function (data, type, row) {
                             data = new Date(data);
-                          
+
                             return data.toISOString().split("T")[0];
                         },
-                        
+
                         title: "Fecha",
                     },
                     {
                         data: "sucursal",
                         render: function (data, type, row) {
-                            return  row.sucursales.Sucursal
+                            return row.sucursales.Sucursal;
                         },
                         title: "sucursal",
                     },
                     { data: "Cliente", title: "código cliente" },
                     { data: "NombreCliente", title: "Nombre Cliente" },
-                    { data: "Importe", title: "Importe" },
+                    { data: "Importe",render:(data, type, row)=>{
+
+                          let valor = new Intl.NumberFormat("es-MX", {
+                                style: "currency",
+                                currency: "MXN", // Puedes cambiar a USD, EUR, etc.
+                            });
+                            return valor.format(data);
+
+
+                    }, title: "Importe" },
                     { data: "Moneda", title: "Moneda" },
-                    { data: "Impuesto", title: "Impuesto" },
-                    { data: "Total",render: function (data, type, row) {
+                    { data: "Impuesto",render:(data, type, row)=>{
+
+                          let valor = new Intl.NumberFormat("es-MX", {
+                                style: "currency",
+                                currency: "MXN", // Puedes cambiar a USD, EUR, etc.
+                            });
+                            return valor.format(data);
+
+
+                    }, title: "Impuesto" },
+                    {
+                        data: "Total",
+                        render: function (data, type, row) {
                             // Calcular el total restando el descuento del importe y sumando el impuesto
-                                return row.Importe-row.Descuento+row.Impuesto;               
-                           
-                    }, title: "Total" },
+                            let valor = new Intl.NumberFormat("es-MX", {
+                                style: "currency",
+                                currency: "MXN", // Puedes cambiar a USD, EUR, etc.
+                            });
+
+                            return valor.format(
+                                row.Importe - row.Descuento + row.Impuesto
+                            );
+                        },
+                        title: "Total",
+                    },
                 ],
                 searchable: true, // Activa la búsqueda en la tabla
                 sortable: true, // Activa el ordenamiento
@@ -63,55 +93,49 @@ function inicializeventasTable(jsonData) {
     }
 }
 
-
-
 async function getventasperiodo(Data) {
     console.log(Data);
-    
+
     let fechaInicial = Data["fechaInicial"];
     let fechaFinal = Data["fechaFinal"];
     let sucursal = Data["sucursal"];
 
     let empresa = JSON.parse(document.getElementById("idempresa").value);
     let periodo = `&$filter=(Id_Empresa eq ${empresa.Id_Empresa})${
-        sucursal !== "" ? ` and Id_Sucursal eq ${sucursal}` : ""} ${
-        fechaInicial != ""
-            ? `and (FechaDoc ge ${new Date(
-                  fechaInicial
-              ).toISOString()} and FechaDoc le ${new Date(
-                  fechaFinal
-              ).toISOString()})&$orderby= FechaDoc`
-            : ""
-    } `;
-  
+        sucursal !== "" ? ` and Id_Sucursal eq ${sucursal}` : ""
+    }`;
+
+    if (fechaInicial != "") {
+        if (fechaInicial) {
+            periodo += ` ${`and (FechaDoc ge ${new Date(
+                fechaInicial
+            ).toISOString()} and FechaDoc le ${new Date(
+                fechaFinal
+            ).toISOString()})&$orderby= FechaDoc`} `;
+        }
+    }
+
     let response = await getVentas(periodo);
     let jsonData = response.data["value"];
-  
-    
+
     inicializeventasTable(jsonData);
 }
 
-
 if (document.getElementById("ventastable") != null) {
-    
-    
 }
-
-
-
 
 let exportExcel = async () => {
     console.log(fechaInicial);
     console.log(fechaFinal);
     let url = `${config.apiUrl}ventas/excel/periodo?initialDate=${fechaInicial}&finalDate=${fechaFinal}&sucursal=${sucursal}`;
-     window.location.href = url;
+    window.location.href = url;
 };
 
 let exportPdf = async () => {
     console.log(fechaInicial);
     console.log(fechaFinal);
     let url = `${config.apiUrl}ventas/pdf/periodo?initialDate=${fechaInicial}&finalDate=${fechaFinal}&sucursal=${sucursal}`;
-     window.location.href = url;
+    window.location.href = url;
 };
 
 let ventasExcel = document.getElementById("ventasExcel");
@@ -130,27 +154,32 @@ if (ventasPdf) {
 
 let formPeriodo = document.getElementById("formPeriodo");
 if (formPeriodo != null) {
-     
-   let fechaInicial=formPeriodo.elements['intialDate'];
-   let fechaFinal=formPeriodo.elements['finalDate'];
-   let sucursal=formPeriodo.elements['filsucursal'];
-   const hoy = new Date();
+    let fechaInicial = formPeriodo.elements["intialDate"];
+    let fechaFinal = formPeriodo.elements["finalDate"];
+    let sucursal = formPeriodo.elements["filsucursal"];
+    const hoy = new Date();
     const anioActual = hoy.getFullYear();
     const mesActual = hoy.getMonth();
 
-    fechaInicial.value = new Date(anioActual, mesActual, 1).toISOString().split("T")[0]; // Primer día del mes
-    fechaFinal.value= new Date(anioActual, mesActual + 1, 0).toISOString().split("T")[0]; // Último día del mes
-   console.log(formPeriodo['filsucursal']);
-   
-     getventasperiodo({"fechaInicial":fechaInicial.value,"fechaFinal":fechaFinal.value,"sucursal":sucursal.value});
+    fechaInicial.value = new Date(anioActual, mesActual, 1)
+        .toISOString()
+        .split("T")[0]; // Primer día del mes
+    fechaFinal.value = new Date(anioActual, mesActual + 1, 0)
+        .toISOString()
+        .split("T")[0]; // Último día del mes
+    console.log(formPeriodo["filsucursal"]);
 
-    formPeriodo.addEventListener("submit",(event)=>{ 
+    getventasperiodo({
+        fechaInicial: fechaInicial.value,
+        fechaFinal: fechaFinal.value,
+        sucursal: sucursal.value,
+    });
+
+    formPeriodo.addEventListener("submit", (event) => {
         event.preventDefault();
-    let fechaInicial = event.target["intialDate"].value;
-    let fechaFinal = event.target["finalDate"].value;
-    let sucursal = event.target["sucursal"].value;
-        getventasperiodo({fechaInicial,fechaFinal,sucursal})});
+        let fechaInicial = event.target["intialDate"].value;
+        let fechaFinal = event.target["finalDate"].value;
+        let sucursal = event.target["sucursal"].value;
+        getventasperiodo({ fechaInicial, fechaFinal, sucursal });
+    });
 }
-
-
-
